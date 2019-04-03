b6222094ca2fb455b86b06e29a6aa77d723866f8
Sergey Ganin <mcheguevara2@gmail.com>
2019-04-03 12:35:54 +0300

    fix

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+class User extends Sequelize.Model {}
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './db/database.sqlite',
+})
+
+User.init(
+  'users',
+  {
+    username: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    hooks: {
+      beforeCreate: user => {
+        const salt = bcrypt.genSaltSync()
+        user.password = bcrypt.hashSync(user.password, salt)
+      },
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password)
+      },
+    },
+  },
+)
+
+// create all the defined tables in the specified database.
+User.sync()
+  .then(() => console.log("users table has been successfully created, if one doesn't exist"))
+  .catch(error => console.log('This error occured', error))
+
+// export User model for use in other files.
+module.exports = User
--- modules/API/index.js
+++ modules/API/index.js
@@ -1,8 +1,8 @@
-const { getAllUsers } = require('../../db')
+// const { getAllUsers } = require('../../db')
 
 function APIHandler(req, res, parsedUrl) {
   console.log(parsedUrl)
-  res.setHeader('Content-Type', 'application/json')
-  return getAllUsers().then(users => res.send(JSON.stringify(users, null, ' ')))
+  // res.setHeader('Content-Type', 'application/json')
+  // return getAllUsers().then(users => res.send(JSON.stringify(users, null, ' ')))
 }
 module.exports = { APIHandler }
