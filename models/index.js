42a2a081934149d8b2d8f703d1a537fe6b30a240
Sergey Ganin <mcheguevara2@gmail.com>
2019-04-12 00:37:11 +0300

    add links

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const sequelize = require('./sequelize-config')
 const User = require('./User')(sequelize)
 const UserGroup = require('./UserGroup')(sequelize)
+const UserGroupLink = require('./UserGroupLink')(sequelize)
 const bcrypt = require('bcrypt')
 const salt = bcrypt.genSaltSync()
 
@@ -11,11 +12,18 @@ const salt = bcrypt.genSaltSync()
 //   through: 'UserGroupLinks',
 //   foreignKey: User.id
 // })
-UserGroup.hasMany(User)
+// UserGroup.hasMany(User)
 // User.belongsToMany(UserGroup, {
 //   through: 'UserGroups'
 // })
 // create all the defined tables in the specified database.
+
+// User.hasMany(UserGroup) // Will add userId to Task model
+// Will also add userId to Task model
+
+UserGroup.belongsToMany(User, { through: 'UserGroupLink' })
+User.belongsToMany(UserGroup, { through: 'UserGroupLink' })
+
 sequelize
   .sync({ force: true })
   .then(() => console.log("users table has been successfully created, if one doesn't exist"))
@@ -32,14 +40,34 @@ sequelize
     })
   )
   .then(() =>
+    User.findOrCreate({
+      where: {
+        name: 'serg',
+        email: 'serg',
+        password: bcrypt.hashSync('serg', salt),
+        isAdmin: true
+      }
+    })
+  )
+  .then(() =>
     User.findOne({
       where: {
         name: 'admin'
       }
     }).then(user => {
-      console.dir(user)
       UserGroup.findAll().then(groups => {
-        Promise.all(groups.map(group => group.addUser(user)))
+        Promise.all(groups.map(group => user.addUserGroup(group)))
+      })
+    })
+  )
+  .then(() =>
+    User.findOne({
+      where: {
+        name: 'serg'
+      }
+    }).then(user => {
+      UserGroup.findAll().then(groups => {
+        Promise.all(groups.map(group => user.addUserGroup(group)))
       })
     })
   )
@@ -47,5 +75,6 @@ sequelize
 
 module.exports = {
   User,
-  UserGroup
+  UserGroup,
+  UserGroupLink
 }
